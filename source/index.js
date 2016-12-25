de26ff9c03157cc6244e512fa167c966cd85a962
Giovanni Calò <giovanni.calo@live.it>
2016-12-25 16:14:03 +0000

    Removed no longer needed plugins

--- source/index.js
+++ source/index.js
@@ -5,9 +5,7 @@ const prefix = (name) => {
 const plugins = [
 	"async-to-generator",
 	"class-properties",
-	"es2015-destructuring",
 	"es2015-modules-commonjs",
-	"exponentiation-operator",
 	"function-bind",
 	"object-rest-spread",
 	"react-jsx"
